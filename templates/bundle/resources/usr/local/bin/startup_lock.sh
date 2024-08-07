8c033c2906aad605917098ca228f86cdfabe7b37
Pedro Henrique Paccola de Oliveira <81725952+pedropaccola@users.noreply.github.com>
2024-08-07 11:40:42 -0300

    Revert "DOCKER-250 Add a randomized delay in between checks"
    This reverts commit c615cff7d67aef1dfb9c18d526c7aaad3f5d12fb.

--- templates/bundle/resources/usr/local/bin/startup_lock.sh
+++ templates/bundle/resources/usr/local/bin/startup_lock.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 function add_lock {
-	echo "Acquiring lock."
-
 	hostname > "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}"
 
-	sleep 2
+	sleep 1
 
 	if [ "$(hostname)" != "$(cat "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}")" ]
 	then
@@ -14,38 +12,28 @@ function add_lock {
 		wait_until_free
 
 		add_lock
-	else
-		echo "Lock acquired by $(hostname)."
 	fi
 }
 
 function wait_until_free {
-	while
-		[ -e "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}" ] &&
-		[ -n "$(cat "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}")" ] &&
-		[ "$(hostname)" != "$(cat "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}")" ]
+	while [ -e "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}" ] && [ "$(hostname)" != "$(cat "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}")" ]
 	do
 		echo "Wait for $(cat "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}") to start up."
 
 		if [ "$(find "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}" -mmin +2)" ]
 		then
-			echo "Lock created by $(cat "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}") was not updated for 2 minutes, unlocking."
+			echo "Lock created by $(cat "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}") was not updated for 2 minutes, removing."
 
-			echo "" > "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}"
-
-			break
+			rm -f "${LIFERAY_CONTAINER_STARTUP_LOCK_FILE}"
 		fi
 
 		sleep 3
 	done
+
+	echo "Acquiring lock."
 }
 
 function main {
-	local delay=$((RANDOM % 10 + 1))
-
-	echo "Delaying lock check for $delay seconds."
-	sleep $delay
-
 	wait_until_free
 
 	add_lock
