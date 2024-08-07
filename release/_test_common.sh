1813bbc00655f96cd9b4e44bc711d49ef72c7614
Pedro Henrique Paccola de Oliveira <81725952+pedropaccola@users.noreply.github.com>
2024-08-07 11:40:42 -0300

    Revert "LPD-31468 Use colors to highlight tests results"
    This reverts commit 7bda0218089cba6fafff258320a2e881113feca3.

--- release/_test_common.sh
+++ release/_test_common.sh
@@ -3,9 +3,9 @@
 function assert_equals {
 	if [ "${1}" = 1 ] || [ "${1}" = true ]
 	then
-		echo -e "${FUNCNAME[1]} \e[1;32mSUCCESS\e[0m"
+		echo "${FUNCNAME[1]} SUCCESS"
 	else
-		echo -e "${FUNCNAME[1]} \e[1;31mFAILED\e[0m"
+		echo "${FUNCNAME[1]} FAILED"
 
 		if [ -n "${2}" ]
 		then
